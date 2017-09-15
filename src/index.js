71ff12274a73ea7cd0fa0128027e1be35e57c424
Franklin Floresca <franklin.jhf@gmail.com>
2017-09-15 11:04:08 -0500

    about to integrate material ui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import promise from 'redux-promise';
 import reducers from './reducers';
 import './index.css';
@@ -19,16 +20,19 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-    <div>
-      <Switch>
-      <Route exact path='/' component={Login} />
-      <Route path='/vehicles/new' component={VehiclePostsNew} />
-      <Route path='/vehicles/:id' component={VehiclePostsShow} />
-      <Route path='/vehicles' component={VehiclePostsIndex} />
-      </Switch>
-    </div>
-    </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+  <MuiThemeProvider>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+      <div>
+        <Switch>
+        <Route exact path='/' component={Login} />
+        <Route path='/vehicles/new' component={VehiclePostsNew} />
+        <Route path='/vehicles/:id' component={VehiclePostsShow} />
+        <Route path='/vehicles' component={VehiclePostsIndex} />
+        </Switch>
+      </div>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>
+  , document.querySelector('.container')
+);
