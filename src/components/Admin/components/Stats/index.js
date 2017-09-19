8ae3816c814492319652178f023b77ee4d73fc5d
greendpd <greendpd@msn.com>
2017-09-19 16:19:46 -0500

    Adding admin pages, modifying routes to allow admin page and to 404 bad requests

---
+++ src/components/Admin/components/Stats/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class stats extends Component{
+  render(){
+    return <div>
+      <h2> Stats Component</h2>
+    </div>
+
+  }
+
+
+}
+
+export default stats;
