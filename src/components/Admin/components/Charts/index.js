675a13df108922d102ec91e57c88e40bbc1a26a1
greendpd <greendpd@msn.com>
2017-09-21 15:30:39 -0500

    adding functionality for dynamic chart

--- src/components/Admin/components/Charts/index.js
+++ src/components/Admin/components/Charts/index.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
+import {Bar,Line} from 'react-chartjs-2';
+import baseUnits from './units';
+import axios from 'axios';
+
+let port=5555;
 
 class charts extends Component{
-  // return <Line data={chartData} options={chartOptions} width="600" height="250"/>
   constructor(props){
     super(props)
     this.state={
@@ -10,48 +13,45 @@ class charts extends Component{
     }
   }
 
-  getChartData(){
+  getChartData(apiData){
     this.setState({
       chartData:{
-        labels:[
-          'Monday','Tuesday','Wednesday','Thursday','Friday'
-        ],
-        datasets:[{
-            label:"Cars Parking",
-            data:[50,27,88,24,33],
+        labels:apiData.labels,
+        datasets:[
+          {
+            label:apiData.datasets[0].label,
+            data:apiData.datasets[0].data,
             lineTension:0,
             backgroundColor:"purple",
             borderColor:"purple",
             fill:false,
           },
           {
-            label:"Cars Leaving",
-            data:[89,10,20,43,57],
+            label:apiData.datasets[1].label,
+            data:apiData.datasets[1].data,
             lineTension:0,
-            backgroundColor:"brown",
-            borderColor:"brown",
+            backgroundColor:"pink",
+            borderColor:"pink",
             fill:false,
-          }
-
-        ]
+          },
+        ],
       }
-
-
     })
   }
 
-  componentWillMount(){
-    this.getChartData();
+  async componentWillMount(){
+    let startTime=new Date('Sept 10 2017');
+    let endTime=new Date();
+    let units=1;
+    const response=await axios.get(`http://localhost:${port}/api/ChartData/${startTime.getTime()}/${endTime.getTime()}?units=86400000&offset=18000000`);
+    this.getChartData(response.data);
   }
 
   render(){
     return <div>
       <Line data={this.state.chartData} />
-
     </div>
-
   }
-
 }
 
 export default charts;
---
+++ src/components/Admin/components/Charts/units.js
@@ -0,0 +1,6 @@
+export default {
+  SECOND:1000,
+  MINUTE:60000,
+  HOUR:3600000,
+  DAY:86400000,
+}
